ca664667f4ae7c956398f99d61aebb5af4d92890
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-05 10:37:00 +0100

    Python Crash Course

---
+++ python.py
@@ -0,0 +1,34 @@
+# from Mary Shelley's Frankenstein
+print("There is something at work in my soul, which I do not understand.")
+
+print("Hello world!")
+
+message_string = "Hello there"
+# Prints "Hello there"
+print(message_string)
+
+# Greeting
+message_string = "Hello there"
+print(message_string)
+ 
+# Farewell
+message_string = "Hasta la vista"
+print(message_string)
+
+print('This message has mismatched quote marks!')
+print('Abracadabra')
+
+an_int = 2
+a_float = 2.1
+ 
+print(an_int + 3)
+# Output: 5
+
+# Prints "500"
+print(573 - 74 + 1)
+ 
+# Prints "50"
+print(25 * 2)
+ 
+# Prints "2.0"
+print(10 / 5)
