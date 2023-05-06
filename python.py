d5ec9fc354df50b8ec153391b6697a8e6b45ee06
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-06 09:47:18 +0100

    Lists Continued

--- python.py
+++ python.py
@@ -21,3 +21,44 @@ items_sold_new = items_sold + ["biscuit", "tart"]
 print(items_sold_new)
 
 my_list + [4]
+
+calls = ["Juan", "Zofia", "Amare", "Ezio", "Ananya"]
+
+print(calls[2])
+
+pancake_recipe = ["eggs", "flour", "butter", "milk", "sugar", "love"]
+
+print(pancake_recipe[-1])
+
+garden = ["Tomatoes", "Green Beans", "Cauliflower", "Grapes"]
+garden[2] = "Strawberries"
+ 
+print(garden)
+
+["Tomatoes", "Green Beans", "Strawberries", "Grapes"]
+
+garden[-1] = "Raspberries"
+ 
+print(garden)
+
+shopping_line = ["Cole", "Kip", "Chris", "Sylvana"]
+
+shopping_line.remove("Chris")
+ 
+print(shopping_line)
+
+# Create a list
+shopping_line = ["Cole", "Kip", "Chris", "Sylvana", "Chris"]
+ 
+# Remove a element
+shopping_line.remove("Chris")
+print(shopping_line)
+
+heights = [["Noelle", 61], ["Ava", 70], ["Sam", 67], ["Mia", 64]]
+
+#A 2d list with three lists in each of the indices. 
+tic_tac_toe = [
+            ["X","O","X"], 
+            ["O","X","O"], 
+            ["O","O","X"]
+]
