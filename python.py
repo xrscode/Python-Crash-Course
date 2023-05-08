ba419ce17b6aab93eef51376981c9ec21fd96e66
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-08 15:50:15 +0100

    Update python.py

--- python.py
+++ python.py
@@ -7,8 +7,36 @@
 # def function_name():
 #   # functions tasks go here
 
-def trip_welcome():
-    print("Welcome to Tripcademy!") 
-    print("Let's get you to your destination.")
+# def trip_welcome():
+#     print("Welcome to Tripcademy!") 
+#     print("Let's get you to your destination.")
 
-trip_welcome()
+# trip_welcome()
+
+# def trip_welcome():
+#   # Indented code is part of the function body
+#   print("Welcome to Tripcademy!") 
+#   print("Let's get you to your destination.")
+ 
+# # Unindented code below is not part of the function body
+# print("Woah, look at the weather outside! Don't walk, take the train!")
+ 
+# trip_welcome()
+
+# def trip_welcome():
+#   print("Welcome to Tripcademy!") 
+#   print("Looks like you're going to Times Square today.")
+ 
+# trip_welcome()
+
+# def trip_welcome(destination):
+#   print("Welcome to Tripcademy!") 
+#   print("Looks like you're going to " + destination + " today.")
+
+#   def my_function(parameter1, parameter2, parameter3):
+#     # Some code
+
+def trip_welcome(origin, destination):
+  print("Welcome to Tripcademy")
+  print("Looks like you are traveling from " + origin)
+  print("And you are heading to " + destination)
