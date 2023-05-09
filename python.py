d59d55e5c163da525b4cbb74088ac2ff0360ae41
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-09 08:03:30 +0100

    Update python.py

--- python.py
+++ python.py
@@ -47,13 +47,72 @@ def calculate_taxi_price(miles_to_travel, rate, discount):
 # 100 is miles_to_travel
 # 10 is rate
 # 5 is discount
-calculate_taxi_price(100, 10, 5)
+# calculate_taxi_price(100, 10, 5)
 
-def calculate_taxi_price(miles_to_travel, rate, discount = 10):
-  print(miles_to_travel * rate - discount )
+# def calculate_taxi_price(miles_to_travel, rate, discount = 10):
+#   print(miles_to_travel * rate - discount )
 
-# Using the default value of 10 for discount.
-calculate_taxi_price(10, 0.5)
+# # Using the default value of 10 for discount.
+# calculate_taxi_price(10, 0.5)
  
-# Overwriting the default value of 10 with 20
-calculate_taxi_price(10, 0.5, 20)
+# # Overwriting the default value of 10 with 20
+# calculate_taxi_price(10, 0.5, 20)
+# Write your code below:
+max_price = max(9.75, 15.50, 5.99, 2.00)
+print(max_price)
+
+min_price = min(9.75, 15.50, 5.99, 2.00)
+print(min_price)
+
+
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
+
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
+
+def trip_welcome(destination):
+  print(" Looks like you're going to the " + destination + " today. ")
+
+budget = 1000
+ 
+# Here we are using a default value for our parameter of `destination` 
+def trip_welcome(destination="California"):
+    print(" Looks like you're going to " + destination)
+    print(" Your budget for this trip is " + str(budget))
+ 
+print(budget)
+trip_welcome()
+
+def calculate_exchange_usd(us_dollars, exchange_rate):
+  return us_dollars * exchange_rate
+ 
+new_zealand_exchange = calculate_exchange_usd(100, 1.4)
+ 
+print("100 dollars in US currency would give you " + str(new_zealand_exchange) + " New Zealand dollars")
+
+weather_data = ['Sunny', 'Sunny', 'Cloudy', 'Raining', 'Snowing']
+ 
+def threeday_weather_report(weather):
+  first_day = " Tomorrow the weather will be " + weather[0]
+  second_day = " The following day it will be " + weather[1]
+  third_day = " Two days from now it will be " + weather[2]
+  return first_day, second_day, third_day
+
+monday, tuesday, wednesday = threeday_weather_report(weather_data)
+ 
+print(monday)
+print(tuesday)
+print(wednesday)
+
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third 
+  
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
