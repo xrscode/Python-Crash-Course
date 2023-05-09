168661a67f79cf3f56ed2b446624b8992fa66105
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-09 17:03:17 +0100

    Update python.py

--- python.py
+++ python.py
@@ -357,3 +357,10 @@
 #       new_lst.append(index)
 #   return new_lst
 # print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# def reversed_list(lst1, lst2):
+#   for index in range(len(lst1)):
+#     if lst1[index] != lst2[len(lst2) - 1 - index]:
+#       return False
+#   return True
+# print(reversed_list([1, 2, 3], [3, 2, 1]))
