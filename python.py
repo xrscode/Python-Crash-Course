d3ab79ad0dcd8df21889aca63c1c1f772561f305
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-09 16:59:20 +0100

    Update python.py

--- python.py
+++ python.py
@@ -350,3 +350,10 @@
 #     return lrg
 # print(max_num([50, -10, 0, 75, 20]))
 
+# def same_values(lst1, lst2):
+#   new_lst = []
+#   for index in range(len(lst1)):
+#     if lst1[index] == lst2[index]:
+#       new_lst.append(index)
+#   return new_lst
+# print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
