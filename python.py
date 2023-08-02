a9529006bfb0b3c1b28f8b128137d10a1b12d3d9
xrscode <120644276+xrscode@users.noreply.github.com>
2023-08-02 15:22:32 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,26 @@
-print('Hello World')
+#print(len(input('What is your name? ')))
+
+# 🚨 Don't change the code below 👇
+#a = input("a: ")
+#b = input("b: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+#c = a
+#d = b
+#a = d
+#b = c
+#Write your code above this line 👆
+####################################
+# 🚨 Don't change the code below 👇
+##print("a: " + a)
+##print("b: " + b)
+
+
+
+
+
+##Project Band Name Generator
+city = input('What city did you grow up in?')
+pet = input('What is your pet\'s name?')
+print('Your band name could be ' + city + ' ' + pet + '.')
